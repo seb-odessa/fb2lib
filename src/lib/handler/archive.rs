f031776d619f37983265f48fa120db0fe9947ed4
seb@kickseed <seb@kickseed>
2018-02-21 08:25:26 +0200

    Archive commands refactored

--- src/lib/handler/archive.rs
+++ src/lib/handler/archive.rs
@@ -4,28 +4,28 @@ use archive;
 use algorithm;
 use result::Fb2Result;
 
-pub fn show_xml(archive_name: &str, file_name: &str) -> Fb2Result<()> {
-    let zip = archive::open(archive_name)?;
-    algorithm::apply(zip, file_name, out::xml)
+pub fn show_xml(archive: &str, book: &str) -> Fb2Result<()> {
+    let zip = archive::open(archive)?;
+    algorithm::apply(zip, book, out::xml)
 }
 
-pub fn show_fb2(archive_name: &str, file_name: &str) -> Fb2Result<()> {
-    let zip = archive::open(archive_name)?;
-    algorithm::apply(zip, file_name, out::fb2)
+pub fn show_fb2(archive: &str, book: &str) -> Fb2Result<()> {
+    let zip = archive::open(archive)?;
+    algorithm::apply(zip, book, out::fb2)
 }
 
-pub fn show_inf(archive_name: &str, file_name: &str) -> Fb2Result<()> {
-    let zip = archive::open(archive_name)?;
-    algorithm::apply(zip, file_name, out::info)
+pub fn show_inf(archive: &str, book: &str) -> Fb2Result<()> {
+    let zip = archive::open(archive)?;
+    algorithm::apply(zip, book, out::info)
 }
 
-pub fn show_zip(archive_name: &str, file_name: &str) -> Fb2Result<()> {
-    let zip = archive::open(archive_name)?;
-    algorithm::apply_to_file(zip, file_name, out::zip_info)
+pub fn show_zip(archive: &str, book: &str) -> Fb2Result<()> {
+    let zip = archive::open(archive)?;
+    algorithm::apply_to_file(zip, book, out::zip_info)
 }
 
-pub fn list_files(archive_name: &str) -> Fb2Result<()> {
-    let mut zip = archive::open(archive_name)?;
+pub fn show_files(archive: &str) -> Fb2Result<()> {
+    let mut zip = archive::open(archive)?;
     for i in 0..zip.len() {
         let file = zip.by_index(i)?;
         out::file_info(&file);
@@ -33,17 +33,17 @@ pub fn list_files(archive_name: &str) -> Fb2Result<()> {
     Ok(())
 }
 
-pub fn check_archive(archive_name: &str, quiet: bool) -> Fb2Result<()> {
+pub fn check_archive(archive: &str, quiet: bool) -> Fb2Result<()> {
     use std::io;
     use std::io::Write;
-    let zip = archive::open(archive_name)?;
+    let zip = archive::open(archive)?;
     let count = zip.len();
     let mut succ = 0;
     let mut curr = 0;
     if !quiet {
         print!("Progress:   %");
     }
-    algorithm::apply_to_xml(zip, "*", |file_name, xml| {
+    algorithm::apply_to_xml(zip, "*", |book, xml| {
         match tools::into_fb2(xml) {
             Ok(_) => succ += 1,
             Err(_) => {
@@ -52,8 +52,8 @@ pub fn check_archive(archive_name: &str, quiet: bool) -> Fb2Result<()> {
                 }
                 println!(
                     "The {} file contained unsupported FB2 file {}",
-                    archive_name,
-                    &file_name
+                    archive,
+                    &book
                 )
             }
         }
--- src/lib/ui/archive.rs
+++ src/lib/ui/archive.rs
@@ -7,61 +7,56 @@ use clap::{App, Arg, SubCommand, ArgMatches};
 pub const CMD: &'static str = "archive";
 const CMD_HELP: &'static str = "Use to work with archives of FB2 books";
 
-const LST: &'static str = "ls";
-const LST_HELP: &'static str = "Print the list of files in the archive";
+const LS: &'static str = "ls";
+const LS_HELP: &'static str = "Print the list of files in the archive";
 const CHECK: &'static str = "check";
 const CHECK_HELP: &'static str = "Try parse all books in archive.";
-const QUIET: &'static str = "QUIET";
+const SHOW: &'static str = "show";
+const SHOW_HELP: &'static str = "Extract and print book description in specified format.";
+
+
+const QUIET: &'static str = "quiet";
 const QUIET_HELP: &'static str = "Perform operation in QUIET mode";
 const XML: &'static str = "xml";
-const XML_HELP: &'static str = "Extract book(s) from archive as FB2 XML.";
+const XML_HELP: &'static str = "Show book's description in a FB2 (XML) format.";
 const FB2: &'static str = "fb2";
-const FB2_HELP: &'static str = "Extract book(s) from archive as FictionBook structure";
+const FB2_HELP: &'static str = "Show book's description as a FictionBook structure";
 const INF: &'static str = "inf";
-const INF_HELP: &'static str = "Extract book(s) brief information from archive";
+const INF_HELP: &'static str = "Show book's brief description";
 const ZIP: &'static str = "zip";
-const ZIP_HELP: &'static str = "Extract book(s) zipped file information from archive";
+const ZIP_HELP: &'static str = "Show book's offet, size and compression method in the rachive";
 
 pub fn add<'a, 'b>(app: App<'a, 'b>) -> App<'a, 'b> {
     let arch = Arg::with_name(ui::ARCH_FILE).help(ui::ARCH_FILE_HELP).required(true);
     let book = Arg::with_name(ui::BOOK_FILE).help(ui::BOOK_FILE_HELP).required(false);
-    let quiet = Arg::with_name(QUIET).help(QUIET_HELP).required(false);
-
+    let quiet = Arg::with_name(QUIET).help(QUIET_HELP).long(QUIET).short("q").required(false);
     app.subcommand(
-        SubCommand::with_name(CMD).about(CMD_HELP).arg(arch)
-        .subcommand(SubCommand::with_name(LST).about(LST_HELP))
-        .subcommand(SubCommand::with_name(CHECK).about(CHECK_HELP).arg(quiet))
-        .subcommand(SubCommand::with_name(XML).about(XML_HELP).arg(book.clone()))
-        .subcommand(SubCommand::with_name(FB2).about(FB2_HELP).arg(book.clone()))
-        .subcommand(SubCommand::with_name(INF).about(INF_HELP).arg(book.clone()))
-        .subcommand(SubCommand::with_name(ZIP).about(ZIP_HELP).arg(book.clone()))
+        SubCommand::with_name(CMD).about(CMD_HELP)
+        .subcommand(SubCommand::with_name(LS).about(LS_HELP).arg(arch.clone()))
+        .subcommand(SubCommand::with_name(CHECK).about(CHECK_HELP).arg(arch.clone()).arg(quiet))
+        .subcommand(
+            SubCommand::with_name(SHOW).about(SHOW_HELP)
+            .subcommand(SubCommand::with_name(XML).about(XML_HELP).arg(arch.clone()).arg(book.clone()))
+            .subcommand(SubCommand::with_name(FB2).about(FB2_HELP).arg(arch.clone()).arg(book.clone()))
+            .subcommand(SubCommand::with_name(INF).about(INF_HELP).arg(arch.clone()).arg(book.clone()))
+            .subcommand(SubCommand::with_name(ZIP).about(ZIP_HELP).arg(arch.clone()).arg(book.clone()))
+        )
     )
 }
 
 pub fn handle<'a>(arg: &ArgMatches<'a>) -> Fb2Result<()> {
-    let archive_name = arg.value_of(ui::ARCH_FILE).unwrap_or("").to_string();
     match arg.subcommand() {
-        (LST, Some(_)) => {
-            handler::archive::list_files(&archive_name)
+        (LS, Some(arg)) => {
+            let archive = arg.value_of(ui::ARCH_FILE).unwrap_or("");
+            handler::archive::show_files(&archive)
         }
         (CHECK, Some(arg)) => {
-            handler::archive::check_archive(&archive_name, arg.occurrences_of(QUIET) != 0)
+            let archive = arg.value_of(ui::ARCH_FILE).unwrap_or("");
+            let quiet = arg.occurrences_of(QUIET) != 0;
+            handler::archive::check_archive(archive, quiet)
         }
-        (XML, Some(arg)) => {
-            let book = arg.value_of(ui::BOOK_FILE).unwrap_or("*");
-            handler::archive::show_xml(&archive_name, book)
-        }
-        (FB2, Some(arg)) => {
-            let book = arg.value_of(ui::BOOK_FILE).unwrap_or("*");
-            handler::archive::show_fb2(&archive_name, book)
-        }
-        (INF, Some(arg)) => {
-            let book = arg.value_of(ui::BOOK_FILE).unwrap_or("*");
-            handler::archive::show_inf(&archive_name, book)
-        }
-        (ZIP, Some(arg)) => {
-            let book = arg.value_of(ui::BOOK_FILE).unwrap_or("*");
-            handler::archive::show_zip(&archive_name, book)
+        (SHOW, Some(arg)) => {
+            handle_show(arg)
         }
         (_, _) => {
             ui::usage(arg)
@@ -69,3 +64,28 @@ pub fn handle<'a>(arg: &ArgMatches<'a>) -> Fb2Result<()> {
     }
 }
 
+fn handle_show<'a>(arg: &ArgMatches<'a>) -> Fb2Result<()> {
+    match arg.subcommand() {
+        (XML, Some(arg)) => {
+            let archive = arg.value_of(ui::ARCH_FILE).unwrap_or("");
+            let book = arg.value_of(ui::BOOK_FILE).unwrap_or("*");
+            handler::archive::show_xml(&archive, book)
+        }
+        (FB2, Some(arg)) => {
+            let archive = arg.value_of(ui::ARCH_FILE).unwrap_or("");
+            let book = arg.value_of(ui::BOOK_FILE).unwrap_or("*");
+            handler::archive::show_fb2(&archive, book)
+        }
+        (INF, Some(arg)) => {
+            let archive = arg.value_of(ui::ARCH_FILE).unwrap_or("");
+            let book = arg.value_of(ui::BOOK_FILE).unwrap_or("*");
+            handler::archive::show_inf(&archive, book)
+        }
+        (ZIP, Some(arg)) => {
+            let archive = arg.value_of(ui::ARCH_FILE).unwrap_or("");
+            let book = arg.value_of(ui::BOOK_FILE).unwrap_or("*");
+            handler::archive::show_zip(&archive, book)
+        }
+        (_, _) => ui::usage(arg)
+    }
+}
